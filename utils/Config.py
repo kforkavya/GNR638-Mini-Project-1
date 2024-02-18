36e1848391ba7c26c33f9e4ec5f6f7c8449a78dd
Kavya Gupta <kavyagupta@Kavyas-MacBook-Pro.local>
2024-02-19 00:05:48 +0530

    First

---
+++ utils/Config.py
@@ -0,0 +1,6 @@
+
+class Config(object):
+    pretrained_path = '/Users/kavyagupta/Desktop/Academics/Sem-4/GNR638/model_pretrained'
+    expansion = 4
+    data_path = '../'
+    model_save_path = '/Users/kavyagupta/Desktop/Academics/Sem-4/GNR638/model_save'
