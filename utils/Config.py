292ed9408cbe5d4ae772361fe0c3b1612d4d5899
Kavya <22b1053@iitb.ac.in>
2024-02-19 04:08:04 +0000

    First

---
+++ utils/Config.py
@@ -0,0 +1,6 @@
+
+class Config(object):
+    pretrained_path = '/home/GNR638-Mini-Project-1/model_pretrained'
+    expansion = 4
+    data_path = '/home/GNR638-Mini-Project-1'
+    model_save_path = '/home/GNR638-Mini-Project-1/model_save'
